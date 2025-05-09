39965165599210b13e72f0e811d3b09cdeb080c1
HHYurdagul <hhyurdagul@gmail.com>
2025-05-09 03:46:00 +0300

    Migrating from nvim-lspconfig to vim.lsp.config

---
+++ nvim/lsp/ruff.lua
@@ -0,0 +1,24 @@
+---@brief
+---
+--- https://github.com/astral-sh/ruff
+---
+--- A Language Server Protocol implementation for Ruff, an extremely fast Python linter and code formatter, written in Rust. It can be installed via `pip`.
+---
+--- Refer to the [documentation](https://docs.astral.sh/ruff/editors/) for more details.
+
+return {
+  cmd = { 'ruff', 'server' },
+  filetypes = { 'python' },
+  root_markers = { 'pyproject.toml', 'ruff.toml', '.ruff.toml', '.git' },
+  settings = {},
+  on_attach = function(client, bufnr)
+    vim.api.nvim_buf_create_user_command(bufnr, 'LspRuffOrganizeImports', function()
+      client:exec_cmd({
+        command = 'ruff.organizeImports',
+        arguments = { vim.uri_from_bufnr(bufnr) },
+      })
+    end, {
+      desc = 'Organize Imports',
+    })
+  end
+}
