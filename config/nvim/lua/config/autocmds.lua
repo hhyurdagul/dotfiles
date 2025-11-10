f68440364caa8cf123b56696cb2f2d0881742c23
HHYurdagul <hhyurdagul@gmail.com>
2025-11-10 22:53:07 +0300

    A lot of changes

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -28,70 +28,69 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	desc = "LSP: Disable hover capability from Ruff",
 })
 
-
 -- AMP
 
 -- Send a quick message to the agent
 vim.api.nvim_create_user_command("AmpSend", function(opts)
-  local message = opts.args
-  if message == "" then
-    print("Please provide a message to send")
-    return
-  end
+	local message = opts.args
+	if message == "" then
+		print("Please provide a message to send")
+		return
+	end
 
-  local amp_message = require("amp.message")
-  amp_message.send_message(message)
+	local amp_message = require("amp.message")
+	amp_message.send_message(message)
 end, {
-  nargs = "*",
-  desc = "Send a message to Amp",
+	nargs = "*",
+	desc = "Send a message to Amp",
 })
 
 -- Send entire buffer contents
 vim.api.nvim_create_user_command("AmpSendBuffer", function(opts)
-  local buf = vim.api.nvim_get_current_buf()
-  local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-  local content = table.concat(lines, "\n")
+	local buf = vim.api.nvim_get_current_buf()
+	local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+	local content = table.concat(lines, "\n")
 
-  local amp_message = require("amp.message")
-  amp_message.send_message(content)
+	local amp_message = require("amp.message")
+	amp_message.send_message(content)
 end, {
-  nargs = "?",
-  desc = "Send current buffer contents to Amp",
+	nargs = "?",
+	desc = "Send current buffer contents to Amp",
 })
 
 -- Add selected text directly to prompt
 vim.api.nvim_create_user_command("AmpPromptSelection", function(opts)
-  local lines = vim.api.nvim_buf_get_lines(0, opts.line1 - 1, opts.line2, false)
-  local text = table.concat(lines, "\n")
+	local lines = vim.api.nvim_buf_get_lines(0, opts.line1 - 1, opts.line2, false)
+	local text = table.concat(lines, "\n")
 
-  local amp_message = require("amp.message")
-  amp_message.send_to_prompt(text)
+	local amp_message = require("amp.message")
+	amp_message.send_to_prompt(text)
 end, {
-  range = true,
-  desc = "Add selected text to Amp prompt",
+	range = true,
+	desc = "Add selected text to Amp prompt",
 })
 
 -- Add file+selection reference to prompt
 vim.api.nvim_create_user_command("AmpPromptRef", function(opts)
-  local bufname = vim.api.nvim_buf_get_name(0)
-  if bufname == "" then
-    print("Current buffer has no filename")
-    return
-  end
+	local bufname = vim.api.nvim_buf_get_name(0)
+	if bufname == "" then
+		print("Current buffer has no filename")
+		return
+	end
 
-  local relative_path = vim.fn.fnamemodify(bufname, ":.")
-  local ref = "@" .. relative_path
-  if opts.line1 ~= opts.line2 then
-    ref = ref .. "#L" .. opts.line1 .. "-" .. opts.line2
-  elseif opts.line1 > 1 then
-    ref = ref .. "#L" .. opts.line1
-  end
+	local relative_path = vim.fn.fnamemodify(bufname, ":.")
+	local ref = "@" .. relative_path
+	if opts.line1 ~= opts.line2 then
+		ref = ref .. "#L" .. opts.line1 .. "-" .. opts.line2
+	elseif opts.line1 > 1 then
+		ref = ref .. "#L" .. opts.line1
+	end
 
-  local amp_message = require("amp.message")
-  amp_message.send_to_prompt(ref)
+	local amp_message = require("amp.message")
+	amp_message.send_to_prompt(ref)
 end, {
-  range = true,
-  desc = "Add file reference (with selection) to Amp prompt",
+	range = true,
+	desc = "Add file reference (with selection) to Amp prompt",
 })
 
 -- Tab size for files
@@ -106,4 +105,6 @@ vim.cmd("autocmd FileType json setlocal shiftwidth=2 softtabstop=2 expandtab")
 vim.cmd("autocmd FileType html setlocal shiftwidth=2 softtabstop=2 expandtab")
 vim.cmd("autocmd FileType css setlocal shiftwidth=2 softtabstop=2 expandtab")
 vim.cmd("autocmd FileType nix setlocal shiftwidth=2 softtabstop=2 expandtab")
+vim.cmd("autocmd FileType lua setlocal shiftwidth=2 softtabstop=2 expandtab")
+vim.cmd("autocmd FileType astro setlocal shiftwidth=2 softtabstop=2 expandtab")
 vim.cmd("autocmd FileType * set formatoptions-=cro")
